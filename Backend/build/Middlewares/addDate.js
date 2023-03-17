b125db6347bcaf0918cc1fcee0c7caa6d4d62a9c
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2023-03-17 01:52:59 +0100

    prueba 2

--- Backend/build/Middlewares/addDate.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const addDateMiddleware = (req, _res, next) => {
-    console.log('Request Type:', req.method);
-    req.requestInstant = new Date();
-    console.log(req.requestInstant);
-    next();
-};
-exports.default = addDateMiddleware;
