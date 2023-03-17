2130493e8b2f13d0535e02c16e2d0c43d68af904
Daniel Castro Martin <109592821+DCM91@users.noreply.github.com>
2023-03-17 21:48:08 +0100

    mejorando loading button!

--- frontend/src/components/AddFilm.tsx
+++ frontend/src/components/AddFilm.tsx
@@ -10,53 +10,75 @@ const AddFilm = (): JSX.Element => {
   const [description, setDescription] = useState("");
   const [image1, setImage1] = useState("");
   const [reload, setReload] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
+  const { post, loading, error, data } = usePost({
+    url: "http://localhost:3001/filmsPost",
+  });
+
+  const handleAddFilm =  () => {
+    try {
+     setReload(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   useEffect(() => {
     if (reload) {
       window.location.reload();
     }
   }, [reload]);
-  const handleAddFilm = () => {
-    setReload(true);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { id, value } = event.target;
+    switch (id) {
+      case "name":
+        setName(value);
+        break;
+      case "year":
+        setYear(value);
+        break;
+      case "price":
+        setPrice(value);
+        break;
+      case "description":
+        setDescription(value);
+        break;
+      case "image":
+        setImage1(value);
+        break;
+    }
   };
-  const { post, loading, error, data } = usePost({
-    url: "http://localhost:3001/filmsPost",
-  });
-  
-  console.log(data);
-  
-  const openModal = () => {
-    setIsOpen(!isOpen);
-  };
-  const handleName = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-    setName(e.target.value);
-  };
-  const handleYear = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-    setYear(e.target.value);
-  }
-  const handlePrice = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-    setPrice(e.target.value);
-  }
-  const handleDescription = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-    setDescription(e.target.value);
-  };
-  const handleImage1 = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-    setImage1(e.target.value);
-  };
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const newFilm = {
-      name,
-      year,
-      price: parseFloat(price),
-      description,
-      image1,
-    };
-    post(newFilm);
-    setIsOpen(false);
-    handleAddFilm();
+    setIsLoading(true);
+    try {
+      const newFilm = {
+        name,
+        year,
+        price: parseFloat(price),
+        description,
+        image1,
+      };
+      await post(newFilm);
+      if (error) {
+        console.log('este es el ERROR', error);
+        window.alert(error);
+
+      } else{ setTimeout(() => {
+        setIsLoading(false);
+        handleAddFilm();
+      }, 2000);}
+    } catch (error) {
+      console.log('este es el ERROR', error);
+      window.alert(error);
+    }
   };
-  if (loading ) return <p>Loading...</p>;
+
+
+
+  if (loading) return <button className="btn loading">loading</button>;
   if (error) return <p>Error</p>;
 
   return (
@@ -64,7 +86,7 @@ const AddFilm = (): JSX.Element => {
       <button
         style={{ margin: "1rem" }}
         className="px-4 py-2 bg-orange-500 hover:bg-orange-600 text-white font-medium rounded-lg shadow-md"
-        onClick={openModal}
+        onClick={() => setIsOpen(true)}
       >
         AddFilm +
       </button>
@@ -84,11 +106,13 @@ const AddFilm = (): JSX.Element => {
               style={{
                 color: "black",
                 background:  "#090a0f",
+                overflowY: "hidden"
               }}
             >
               <h3 className="font-bold text-2xl text-red-500 mb-4">
                 Agrega una película
               </h3>
+              {isLoading && <div className="spinner">Cargando...</div>}
               <form onSubmit={handleSubmit} className="form">
                 <div className="mb-4">
                   <label
@@ -102,9 +126,10 @@ const AddFilm = (): JSX.Element => {
                     id="name"
                     value={name}
                     placeholder="Spiderman 2"
-                    onChange={handleName}
+                    onChange={handleChange}
                     className="input input-success w-full"
                   />
+                  {name === '' && <p className="text-red-500">Required</p>}
                 </div>
                 <div className="mb-4">
                   <label
@@ -118,9 +143,10 @@ const AddFilm = (): JSX.Element => {
                     id="year"
                     value={year}
                     placeholder="1999"
-                    onChange={handleYear}
+                    onChange={handleChange}
                     className="input input-success w-full"
                   />
+                  {year === '' && <p className="text-red-500">Required</p>}
                 </div>
                 <div className="mb-4">
                   <label
@@ -134,9 +160,10 @@ const AddFilm = (): JSX.Element => {
                     id="price"
                     value={price}
                     placeholder= "10"
-                    onChange={handlePrice}
+                    onChange={handleChange}
                     className="input input-success w-full"
                   />
+                  {price === '' && <p className="text-red-500">Required</p>}
                 </div>
                 <div className="mb-4">
                   <label
@@ -150,7 +177,7 @@ const AddFilm = (): JSX.Element => {
                     className="textarea textarea-success w-full"
                     value={description}
                     placeholder= "Añade tu descripción"
-                    onChange={handleDescription}
+                    onChange={handleChange}
                   ></textarea>
                 </div>
                 <div className="mb-4">
@@ -164,22 +191,25 @@ const AddFilm = (): JSX.Element => {
                     type="url"
                     id="image"
                     value={image1}
-                    onChange={handleImage1}
+                    onChange={handleChange}
                     className="input input-success w-full"
                   />
+                  {image1 === '' && <p className="text-red-500">Required</p>}
                 </div>
                 <div className="modal-action">
-                  <button type="submit" onClick={handleAddFilm} className="btn btn-primary">
-                    Agregar
-                  </button>
-                  
+                  {data ? (
+                    <button className="btn loading bg-orange-500 hover:bg-orange-600">loading</button>
+                  ) : (
+                    <button type="submit" className="btn bg-orange-500 hover:bg-orange-600">
+                      Agregar
+                    </button>
+                  )}
                   <label
                     htmlFor="my-modal"
                     className="btn btn-active bg-red-500 hover:bg-red-600"
                   >
                     Cerrar
                   </label>
-                  {data && <p>Película agregada exitosamente.</p>}
                 </div>
               </form>
             </div>
